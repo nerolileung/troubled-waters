2532e8d45ceda6df6a6cf4865dd7f9ff979b1ea4
CptPuddleboat <64780495+CptPuddleboat@users.noreply.github.com>
2021-03-24 13:06:19 +0000

    changes to the main game scene, and some more animation work

---
+++ Assets/Scripts/InteractableFilter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableFilter : Interactable
+{
+    public GameManager gameManager;
+
+    public override bool Interact()
+    {
+        gameManager.CleanClick();
+        return true;
+    }
+}
---
+++ Assets/Scripts/InteractableRiver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableRiver : Interactable
+{
+    public GameManager gameManager;
+
+    public override bool Interact()
+    {
+        gameManager.DirtyClick();
+        return true;
+    }
+}
